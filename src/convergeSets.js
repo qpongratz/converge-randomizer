f96ffa0eba683e8b214922d41e0f7d85fc836d9f
Quentin Pongratz <qpongratz@gmail.com>
2025-07-27 19:03:21 -0500

    put random choices on screen

---
+++ src/convergeSets.js
@@ -0,0 +1,64 @@
+export const Sets = [
+  {
+    name: "Catalysts of Change",
+    skillsets: [
+      {
+        name: "Archivists",
+        targetScore: 5,
+        game: "Catalysts of Change"
+      },
+      {
+        name: "Mechanists",
+        targetScore: 3,
+        game: "Catalysts of Change"
+      },
+      {
+        name: "Alchemists",
+        targetScore: 4,
+        game: "Catalysts of Change"
+      }
+    ]
+  },
+  {
+    name: "Sparks of Hope",
+    skillsets: [
+      {
+        name: "Delvers",
+        targetScore: 2,
+        game: "Sparks of Hope"
+      },
+      {
+        name: "Astropaths",
+        targetScore: 4,
+        game: "Sparks of Hope"
+      },
+      {
+        name: "Healers",
+        targetScore: 4,
+        game: "Sparks of Hope"
+      }
+    ]
+  },
+  {
+    name: "Engines of Progress",
+    skillsets: [
+      {
+        name: "Protectors",
+        targetScore: 5,
+        game: "Engines of Progress"
+      },
+      {
+        name: "Merchants",
+        targetScore: 3,
+        game: "Engines of Progress"
+      },
+      {
+        name: "Searchers",
+        targetScore: 3,
+        game: "Engines of Progress"
+      }
+    ]
+  }
+]
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 import "./styles.css";
+import {Sets} from "./convergeSets.js"
 
+const checkboxes = document.getElementById('checkboxes');
+const content = document.getElementById('content')
+
+function createCheckbox(convergeSet) {
+  let checkbox = document.createElement("input")
+  checkbox.type = 'checkbox'
+  checkbox.innerText = convergeSet.name
+  checkboxes.appendChild(checkbox)
+}
+
+Sets.forEach(createCheckbox);
+
+
+
+function getSkillsets(chosenGames) {
+  return chosenGames.map((g) => g.skillsets).flat()
+}
+
+function chooseSkillsets(sets) {
+  let chosenSkillsets = []
+  while (chosenSkillsets.length < 3) {
+    let choice = Math.floor( Math.random() * sets.length);
+    let chosen = sets.at(choice)
+    if (!chosenSkillsets.includes(chosen)) {
+      chosenSkillsets.push(chosen)
+    }
+  }
+  return chosenSkillsets
+}
+
+function displayResults(array) {
+  console.log(array)
+  array.forEach((element) => {
+    let skill = document.createElement("p")
+    skill.innerText = `Game: ${element.game} Skillset: ${element.name} Target Score: ${element.targetScore}`
+    content.appendChild(skill);
+  })
+}
+
+
+let skillsets = getSkillsets(Sets);
+console.log(skillsets)
+displayResults(chooseSkillsets(skillsets));
